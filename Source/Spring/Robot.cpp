5c43d140c5752220f40ececbfcb46a32aa226a85
Nanuli Mäntylä <morndulin@hotmail.com>
2024-01-12 16:00:02 +0200

    added inputs to player, mesh to robotC++, animBp, interactables

--- Source/Spring/Robot.cpp
+++ Source/Spring/Robot.cpp
@@ -2,12 +2,25 @@
 
 
 #include "Robot.h"
+#include "UObject/ConstructorHelpers.h"
 
 // Sets default values
 ARobot::ARobot()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> robotMesh(TEXT("SkeletalMesh'/Game/Characters/Robots/K9/SKM_K9.SKM_K9'"));
+
+	if (robotMesh.Object)
+	{
+		TArray<USkeletalMeshComponent*> skeletalMeshes;
+		GetComponents(skeletalMeshes, false);
+		if (skeletalMeshes.Num() > 0)
+		{
+			skeletalMeshes[0]->SetSkeletalMesh(robotMesh.Object);
+		}
+	}
 
 }
 
