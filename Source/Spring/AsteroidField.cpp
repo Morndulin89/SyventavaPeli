ce0f9667389af6b65731f481bf692a24c2d889d9
Nanuli Mäntylä <morndulin@hotmail.com>
2024-02-20 19:14:26 +0200

    working on asteroidfield, dialogtree

---
+++ Source/Spring/AsteroidField.cpp
@@ -0,0 +1,70 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AsteroidField.h"
+
+// Sets default values
+AAsteroidField::AAsteroidField()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	triggerBox = CreateDefaultSubobject<UBoxComponent>(TEXT("RootComponent"));	//luodaan UBoxcomponent 
+	RootComponent = triggerBox;													//Set it as rootcomponent
+}
+
+// Called when the game starts or when spawned
+void AAsteroidField::BeginPlay()
+{
+	Super::BeginPlay();
+
+	triggerBox->OnComponentEndOverlap.AddDynamic(this, &AAsteroidField::DestroyAsteroid);
+
+	for(int i = 0; i < maxAsteroidsInField; i++)
+	{
+		FVector boxExtents = triggerBox->GetScaledBoxExtent() * 0.5;
+		FVector spawnLocation = this->GetActorLocation() + 
+			FVector(FMath::RandRange(-boxExtents.X, boxExtents.X),
+					FMath::RandRange(-boxExtents.Y, boxExtents.Y),
+					FMath::RandRange(-boxExtents.Z, boxExtents.Z));
+
+		FRotator spawnRotation = FRotator::ZeroRotator;
+
+		AAsteroid* newAsteroid = GetWorld()->SpawnActor<AAsteroid>(tyyppi, spawnLocation, spawnRotation);
+
+		UPrimitiveComponent* pc = newAsteroid->FindComponentByClass<UPrimitiveComponent>();
+		newAsteroid->Tags.Add(FName("asteroid"));
+
+		//random velocity to asteroid 
+		FVector velocity = FVector(	FMath::RandRange(-minVelocity.X, maxVelocity.X),
+									FMath::RandRange(-minVelocity.Y, maxVelocity.Y),
+									FMath::RandRange(-minVelocity.Z, maxVelocity.Z));
+
+
+		pc->SetPhysicsLinearVelocity(velocity, true, NAME_None);					//laitetaan asteroidit liikkeelle
+
+		asteroids.Add(newAsteroid);
+
+	}
+	
+}
+
+// Called every frame
+void AAsteroidField::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AAsteroidField::DestroyAsteroid(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	//check we destroy only asteroids on the endOverlap
+	if (OtherActor->Tags.Contains(FName("asteroid"))) 
+	{
+		OtherActor->Destroy();
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("not asteroid overlapped"));
+	}
+	
+}
---
+++ Source/Spring/AsteroidField.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Asteroid.h"
+#include "Components/BoxComponent.h"
+#include "AsteroidField.generated.h"
+
+UCLASS()
+class SPRING_API AAsteroidField : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AsteroidField")
+	UBoxComponent* triggerBox;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AsteroidField")
+	int maxAsteroidsInField;
+
+	UPROPERTY()
+	TArray<AAsteroid*> asteroids;				
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AsteroidField")
+	TSubclassOf<AAsteroid> tyyppi; 
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AsteroidField")
+	FVector minVelocity; 
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AsteroidField")
+	FVector maxVelocity;
+
+
+	// Sets default values for this actor's properties
+	AAsteroidField();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+	UFUNCTION()
+	void DestroyAsteroid(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+
+};
